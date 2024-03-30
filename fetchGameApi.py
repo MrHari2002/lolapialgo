b0ba2a57d4fb592a7c4d6aaa6bb360d2ac22dbde
Hengrui Jia <hari20020511@gmail.com>
2024-03-29 22:29:07 -0400

    1

--- fetchGameApi.py
+++ fetchGameApi.py
@@ -6,12 +6,22 @@ def fetch_game_stats():
     """Fetch game stats from the provided URL."""
     try:
         # Bypass SSL verification for HTTPS - use with caution and consider security implications.
-        response = requests.get('https://127.0.0.1:2999/liveclientdata/activeplayer', verify=False)
+        response = requests.get('https://127.0.0.1:2999/liveclientdata/playerlist', verify=False)
         return response
     except requests.exceptions.RequestException as e:
         print("Request failed:", e)
         return None
 
+def fetch_ative_player():
+    """Fetch game stats from the provided URL."""
+    try:
+        # Bypass SSL verification for HTTPS - use with caution and consider security implications.
+        response = requests.get('https://127.0.0.1:2999/liveclientdata/playerlist', verify=False)
+        return response
+    except requests.exceptions.RequestException as e:
+        print("Request failed:", e)
+        return None
+    
 def write_stats_to_file(game_stats):
     """Append the fetched game stats to a file."""
     with open("game_stats.txt", "a") as file:  # 'a' mode for appending data
@@ -19,8 +29,11 @@ def write_stats_to_file(game_stats):
         file.write("\n\n New Data")  # Add some space between entries for readability
 
 def main():
+    active_player = ""
+
     while True:
         response = fetch_game_stats()
+        
         if response and response.status_code == 200:
             game_stats = response.json()
             write_stats_to_file(game_stats)
